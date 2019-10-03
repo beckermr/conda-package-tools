3cf7871c31b39379220a7f2ce56db14b90cfbee1
Matthew R Becker <becker.mr@gmail.com>
2019-10-03 15:51:02 -0500

    try again

--- build.sh
+++ build.sh
@@ -18,12 +18,18 @@ cat conda-package-tools/condarc > $HOME/.condarc
 conda build -m conda-package-tools/${CONFIG}.yaml recipe
 
 
+echo "Uploading the package..."
+
 if [ "$BUILD_SOURCEBRANCHNAME" == "master" ]
 then
     if [ "$BUILD_REPOSITORY_NAME" == "beckermr/conda-package-tools" ]
     then
-        # remove the old package
-        anaconda -t ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
+        {
+            # remove the old package
+            anaconda -t ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
+        } || {
+            echo "WARNING: could not remove old build for testing on master!"
+        }
     fi
     anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/*/*.tar.bz2
 fi
