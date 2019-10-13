627dfc20782ac24d501cca4f66c1b3e58274bfac
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-10-13 09:22:01 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -48,29 +48,29 @@ if [[ `compgen -G ${HOME}/miniconda/conda-bld/*/*.tar.bz2` ]]; then
     echo "package sizes:"
     du -h ${HOME}/miniconda/conda-bld/*/*.tar.bz2
     echo " "
-fi
 
-if [[ ${ANACONDA_TOKEN} ]]; then
-    # https://stackoverflow.com/questions/24318927/bash-regex-to-match-semantic-version-number
-    SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
+    if [[ ${ANACONDA_TOKEN} ]]; then
+        # https://stackoverflow.com/questions/24318927/bash-regex-to-match-semantic-version-number
+        SEMVER_REGEX="^(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)\\.(0|[1-9][0-9]*)(\\-[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?(\\+[0-9A-Za-z-]+(\\.[0-9A-Za-z-]+)*)?$"
 
-    if [ "$BUILD_REPOSITORY_NAME" == "beckermr/conda-package-tools" ]; then
-        {
-            # remove the old package
-            anaconda -t ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
-        } || {
-            echo "WARNING: could not remove old build for testing!"
-        }
-        echo "Uploading the package..."
-        anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/*/*.tar.bz2
-        exit 0
-    else
-        if [[ "$BUILD_SOURCEBRANCHNAME" == "master" ]] || [[ ${BUILD_SOURCEBRANCHNAME#v} =~ $SEMVER_REGEX ]]; then
+        if [ "$BUILD_REPOSITORY_NAME" == "beckermr/conda-package-tools" ]; then
+            {
+                # remove the old package
+                anaconda -t ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
+            } || {
+                echo "WARNING: could not remove old build for testing!"
+            }
             echo "Uploading the package..."
             anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/*/*.tar.bz2
             exit 0
+        else
+            if [[ "$BUILD_SOURCEBRANCHNAME" == "master" ]] || [[ ${BUILD_SOURCEBRANCHNAME#v} =~ $SEMVER_REGEX ]]; then
+                echo "Uploading the package..."
+                anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniconda/conda-bld/*/*.tar.bz2
+                exit 0
+            fi
         fi
     fi
+    echo "Could not upload the package due to missing anaconda token or non-matching branch name!"
 fi
 
-echo "Could not upload the package due to missing anaconda token or wrong branch name!"
