64b406d041701e3343dbc6936d1b57a8a2a36f15
Matthew R Becker <beckermr@users.noreply.github.com>
2020-06-19 16:59:41 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -56,12 +56,12 @@ if [[ `compgen -G ${HOME}/miniforge3/conda-bld/*/*.tar.bz2` ]]; then
         if [ "$BUILD_REPOSITORY_NAME" == "beckermr/conda-package-tools" ]; then
             {
                 # remove the old package
-                anaconda -t ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
+                anaconda --token ${ANACONDA_TOKEN} remove -f beckermr/test_package/0.1.0/${os}-64/test_package-0.1.0-py${pyver}_0.tar.bz2
             } || {
                 echo "WARNING: could not remove old build for testing!"
             }
             echo "Uploading the package..."
-            anaconda -t ${ANACONDA_TOKEN} upload $HOME/miniforge3/conda-bld/*/*.tar.bz2
+            anaconda --token ${ANACONDA_TOKEN} upload $HOME/miniforge3/conda-bld/*/*.tar.bz2
             exit 0
         else
             if [[ "$BUILD_SOURCEBRANCHNAME" == "master" ]] || [[ ${BUILD_SOURCEBRANCHNAME#v} =~ $SEMVER_REGEX ]]; then
